aa1974780283e76fab416bd96bd712e39475de7f
Ruslan Bondar <Ruslan_Bondar@epam.com>
2018-07-18 18:27:06 +0300

    AzureSQL fix

--- AzureSQL.Recommender.Import/Data/DataProvider.cs
+++ AzureSQL.Recommender.Import/Data/DataProvider.cs
@@ -1,19 +1,15 @@
 ﻿using AzureSQL.Recommender.Import.Data.Entities;
 using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
 
 namespace AzureSQL.Recommender.Import.Data
 {
     public static class DataProvider
     {
-        private static readonly string _generatedDataLocation =
-            ConfigurationManager.AppSettings["DataForImport"];
-
-        public static List<Brand> ReadBrands(string sampleName)
+        public static List<Brand> ReadBrands(string generatedDataLocation, string sampleName)
         {
             var brands = new List<Brand>();
-            var path = Path.Combine(_generatedDataLocation, $"{sampleName}-brands.csv");
+            var path = Path.Combine(generatedDataLocation, $"{sampleName}-brands.csv");
             using (var sr = new StreamReader(path))
             {
                 string str;
@@ -27,31 +23,37 @@ namespace AzureSQL.Recommender.Import.Data
             return brands;
         }
 
-        public static List<Product> ReadProducts(string sampleName)
+        public static List<Product> ReadProducts(string generatedDataLocation, string sampleName)
         {
             var products = new List<Product>();
-            var path = Path.Combine(_generatedDataLocation, $"{sampleName}-products.csv");
+            var path = Path.Combine(generatedDataLocation, $"{sampleName}-products.csv");
             using (var sr = new StreamReader(path))
             {
                 string str;
                 while ((str = sr.ReadLine()) != null)
                 {
                     var tokens = str.Split(';');
-                    products.Add(new Product() { Id = int.Parse(tokens[0]), Name = tokens[2], BrandId = int.Parse(tokens[1]) });
+                    products.Add(new Product() {
+                        Id = int.Parse(tokens[0]),
+                        Name = tokens[2],
+                        BrandId = int.Parse(tokens[1])
+                    });
                 }
             }
 
             return products;
         }
 
-        public static (List<Person> people, List<Order> orders) ReadPeopleAndOrders(string sampleName)
+        public static (List<Person> people, List<Order> orders) ReadPeopleAndOrders(
+            string generatedDataLocation, 
+            string sampleName)
         {
             var people = new List<Person>();
             var orders = new List<Order>();
 
             int orderdId = 0;
 
-            var path = Path.Combine(_generatedDataLocation, $"{sampleName}-people.csv");
+            var path = Path.Combine(generatedDataLocation, $"{sampleName}-people.csv");
             using (var sr = new StreamReader(path))
             {
                 string str;
